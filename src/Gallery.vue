9d2ed1e6646bf1de5651bc2300d69af7cb6ea432
Annei <33969739+Annei@users.noreply.github.com>
2018-11-25 17:32:55 -0500

    updates
    no abrir rimeline

---
+++ src/Gallery.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="page-gallery scrollable only-y">
+		<div class="page-header header-accent card-base card-shadow--small">
+			<h1>Galería</h1>
+			<el-breadcrumb separator="/">
+				<el-breadcrumb-item :to="{ path: '/' }"><i class="mdi mdi-home-outline"></i></el-breadcrumb-item>
+				<el-breadcrumb-item>Aplicaciones</el-breadcrumb-item>
+				<el-breadcrumb-item>Galeria</el-breadcrumb-item>
+			</el-breadcrumb>
+			<h4>Todas las fotos fueron tomadas <a href="https://pexels.com/" target="_blank" class="white-text">stickynsweet.com</a></h4>
+		</div>
+
+		<div class="photo-list">
+			<profile-gallery></profile-gallery>
+		</div>
+	</div>
+</template>
+
+<script>
+import ProfileGallery from '@/components/Profile/ProfileGallery'
+
+export default {
+	name: 'Gallery',
+	mounted() {
+		//console.log('Gallery mounted')
+	},
+	components: {
+		ProfileGallery
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+/*.photo-list {
+	//padding: 20px;
+}*/
+</style>
+
---
+++ src/Timeline.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="page-timeline scrollable only-y">
+		<div class="page-header">
+			<h1>Chat</h1>
+			
+		</div>
+	</div>
+</template>
+
+<script>
+import { Timeline, TimelineItem, TimelineTitle } from 'vue-cute-timeline'
+
+export default {
+	name: 'TimelinePage',
+	mounted() {
+		//console.log('Timeline mounted')
+	},
+	components: {
+		Timeline,
+		TimelineItem,
+		TimelineTitle
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/_variables';
+
+.timeline-box {
+	padding: 50px;
+}
+.timeline {
+	max-width: 1200px;
+	margin: 0px auto;
+	font-family: inherit;
+}
+.timeline, .timeline-title {
+	color: $text-color;
+	line-height: 1.4;
+	cursor: default;
+}
+.append {
+    font-size: .8em;
+    margin-top: 3px;
+    color: transparentize($text-color, .5);
+}
+.icon-time {
+	width: 32px;
+	margin-top: 2px;
+	border-radius: 50%;
+}
+</style>
+
